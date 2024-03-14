a34948014f4a2a8d5b381a604cbd264665781cb8
Nima <100118467+NimaWasTaken@users.noreply.github.com>
2024-03-14 15:05:51 +0330

    Update bank system with struct data type

---
+++ 15th-Session/bank.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct User
+{
+	int id;
+	int balance;
+};
+
+bool check(vector<User> information, int id) {
+	for (int i = 0; i < information.size(); i++) {
+		if (information[i].id == id) {
+			return true;
+		}
+	}
+	return false;
+}
+
+vector<User> deposit(vector<User> information, int id, int amount) {
+	for (int i = 0; i < information.size(); i++) {
+		if (information[i].id == id) {
+			information[i].balance += amount;
+			cout << "OK. ";
+			return information;
+		}
+	}
+}
+
+vector<User> withdraw(vector<User> information, int id, int amount) {
+	for (int i = 0; i < information.size(); i++) {
+		if (information[i].id == id) {
+			if (amount > information[i].balance) {
+				cout << "Not enough money. ";
+				return information;
+			}
+			else {
+				information[i].balance -= amount;
+				cout << "OK. ";
+				return information;
+			}
+		}
+	}
+	cout << "Invalid Account. ";
+	return information;
+}
+
+void balance(vector<User> information, int id) {
+	for (int i = 0; i < information.size(); i++) {
+		if (information[i].id == id) {
+			cout << "Account balance: " << information[i].balance;
+			return;
+		}
+	}
+	cout << "Invalid Account. ";
+}
+
+void init() {
+	vector<User> information;
+
+	while (true)
+	{
+		string cmd;
+		int id;
+		int amount;
+
+		cin >> cmd;
+		if (cmd == "deposit") {
+			cin >> id;
+			cin >> amount;
+			if (check(information, id)) {
+				information = deposit(information, id, amount);
+				cout << endl;
+			}
+			else {
+				User user;
+				user.id = id;
+				user.balance = amount;
+				information.push_back(user);
+				cout << "OK. ";
+				cout << endl;
+			}
+		}
+		else if (cmd == "withdraw") {
+			cin >> id;
+			cin >> amount;
+			information = withdraw(information, id, amount);
+			cout << endl;
+		}
+		else if (cmd == "balance") {
+			cin >> id;
+			balance(information, id);
+			cout << endl;
+		}
+	}
+}
+
+int main() {
+	init();
+}
